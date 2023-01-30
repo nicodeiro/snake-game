dd1b6a36ec071dddd78076459aa79dd6f018fc5a
Nicolas Cordeiro <87909401+nicodeiro@users.noreply.github.com>
2023-01-30 23:12:36 +0100

    Add files via upload

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,11 +1,14 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Arial", 24, "normal")
+FONT = ("Arial", 14, "bold")
+
 
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -13,13 +16,21 @@ class Score(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #   self.goto(0, 0)
+    #   self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -17,7 +17,7 @@ class Snake:
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
-        self.segments[0].shape("circle")
+        # self.segments[0].shape("circle")
 
     def add_segment(self, position):
         new_segment = Turtle("square")
@@ -25,6 +25,13 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
